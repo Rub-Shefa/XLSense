d2967bec802245ac29962520b00031f827802f73
Labib-BD <bestlabib@gmail.com>
2026-03-03 13:23:03 +0600

    Added login/register/logout/dashboard routes

--- automation/urls.py
+++ automation/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard_view, name='dashboard_home'),
-    path('dashboard/', views.dashboard_view, name='dashboard'),
-    path('manage-templates/', views.manage_templates_view, name='manage_templates'), # New path
+    path('', views.login_view, name='login'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    
+    path('dashboard/', views.user_dashboard_view, name='user_dashboard'),
+    path('admin/', views.admin_dashboard_view, name='admin_dashboard'),
+    path('manage-templates/', views.manage_templates_view, name='manage_templates'),
 ]
